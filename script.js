1598a3d72bf618651f27458b8bc2d27e142bfcb3
Muhd Dandarawie Bin Rohani <s10208266@connect.np.edu.sg>
2021-02-02 16:52:45 +0800

    GET from API Done. activates on firstpage.html

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    var url = "https://disease.sh/v3/covid-19/countries/Singapore" //api and search function
+
+    $.ajax({
+        method:'GET',
+        url:url,
+        dataType:"JSON",
+    
+        statusCode: {
+            404: function () {
+      
+                alert("Check Spelling!");
+            }
+        },
+        
+    
+        success:function(data){
+            console.log(data)
+            $('#fact_id').html('');
+    
+            $('#fact_id').append('<li>Total Cases: '+data.cases+'</li>');
+
+            $('#fact_id').append('<li>Current Infected: '+data.active+'</li>');
+
+            $('#fact_id').append('<li>Total Deaths: '+data.deaths+'</li>');
+
+            $('#fact_id').append('<li>Total Recovered: '+data.recovered+'</li>');
+
+            $('#fact_id').append('<li>Total infected today: '+data.cases+'</li>');
+        }
+    })
+})
+
+
