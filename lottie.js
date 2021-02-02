959aac21ed1f612829d4dd70114850c3f8276063
Muhd Dandarawie Bin Rohani <s10208266@connect.np.edu.sg>
2021-02-02 16:58:43 +0800

    changednameof firstpagetosecondpage.scriptchanges

--- lottie.js
+++ lottie.js
@@ -5,5 +5,5 @@ player.load('https://assets8.lottiefiles.com/datafiles/wW9k6ALg5Mn9cIj/data.json
 setTimeout(myFunction, 5000);
 
 function myFunction(){
-    window.location.href = "firstpage.html";
+    window.location.href = "secondpage.html";
   }
