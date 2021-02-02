eac90a1dab5843accb8538e853117b40d2c04ec3
Muhd Dandarawie Bin Rohani <s10208266@connect.np.edu.sg>
2021-02-02 15:25:43 +0800

    lottie loading in index page to firstpage

---
+++ lottie.js
@@ -0,0 +1,9 @@
+const player=
+document.querySelector('lottie-player');
+player.load('https://assets8.lottiefiles.com/datafiles/wW9k6ALg5Mn9cIj/data.json')
+
+setTimeout(myFunction, 5000);
+
+function myFunction(){
+    window.location.href = "firstpage.html";
+  }
